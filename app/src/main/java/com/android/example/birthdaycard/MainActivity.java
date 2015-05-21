bc1e6060d4c6c261b2ab99bb1d4aaa5c44a5097c
Lyla <lyla@udacity.com>
2015-05-21 02:14:52 -0700

    Upgrade gradle and MainActivity files

--- app/src/main/java/com/android/example/birthdaycard/MainActivity.java
+++ app/src/main/java/com/android/example/birthdaycard/MainActivity.java
@@ -1,10 +1,16 @@
 package com.android.example.birthdaycard;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 
-
-public class MainActivity extends ActionBarActivity {
+/**
+ * This activity is the activity for the Birthday Card App.
+ *
+ * Note that this version uses AppCompatActivity, which is slightly different from the filmed
+ * version used in the videos for Android Development for Beginners. The old version used the
+ * now deprecated ActionBarActivity
+ */
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +18,5 @@ public class MainActivity extends ActionBarActivity {
         setContentView (R.layout.activity_main);
 
     }
-
-
-
 
 }
