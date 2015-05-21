20c9d183d7182458c02f9f4e07093c5098eea0a8
Lyla <lyla@udacity.com>
2015-05-21 02:05:09 -0700

    Commit current state

--- app/src/main/java/com/android/example/birthdaycard/MainActivity.java
+++ app/src/main/java/com/android/example/birthdaycard/MainActivity.java
@@ -9,7 +9,11 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView (R.layout.activity_main);
+
     }
+
+
+
 
 }
